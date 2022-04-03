80c38aeaf84f0a2eda7543d73dc6cb4e87696d4c
DESKTOP-MM4VO2S\zhao <1946098283@qq.com>
2022-04-03 18:11:56 +0800

    add cors

---
+++ README.md
@@ -0,0 +1,25 @@
+## 配置文件yaml解析
+- spf13/viper
+
+## 日志工具
+- logrus
+- file-rotatelogs
+- lfshook
+
+## 用户密码存储
+- **md5加盐加密** (目前使用的)
+- bcrypt
+- scrypt（专家方案）
+
+## 用户身份认证
+- jwt-go
+
+## 表单数据验证
+- go-playground/validator (gin的默认验证器)
+
+
+## 文件上传
+- qiniu 
+
+## 跨域配置
+- gin-contrib/cors
---
+++ middleware/cors.go
@@ -0,0 +1,32 @@
+package middleware
+
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"time"
+)
+
+// Cors 跨域配置
+func Cors() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		cors.New(cors.Config{
+
+			AllowOrigins: []string{"*"},
+
+			AllowMethods: []string{"*"},
+
+			AllowHeaders: []string{"Origin"},
+
+			ExposeHeaders: []string{"Content-Length", "Authorization"},
+
+			// 可选字段是否允许发送cookie,这个值也只能设为true，如果服务器不要浏览器发送Cookie，删除该字段。
+			// AllowCredentials: true,
+
+			//AllowOriginFunc: func(origin string) bool {
+			//	return origin == "https://github.com"
+			//},
+
+			MaxAge: 12 * time.Hour,
+		})
+	}
+}
